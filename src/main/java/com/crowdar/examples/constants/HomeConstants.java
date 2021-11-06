b59d88b21a9880db10dba4f58c2961c6a6a5d9e5
Luna Maira Sauri <88466827+Mooninaction@users.noreply.github.com>
2021-11-06 16:46:00 -0300

    Code completed. Second Scenario Error:0

--- src/main/java/com/crowdar/examples/constants/HomeConstants.java
+++ src/main/java/com/crowdar/examples/constants/HomeConstants.java
@@ -3,7 +3,7 @@ package com.crowdar.examples.constants;
 public class HomeConstants {
 
     public static final String TIME_ENTRIES_TEXT = "ID:me.clockify.android:id/toolbar_title";
-    public static final String HAMBURGER_MENU_DRAWER = "xpath://android.widget.ImageButton[@content-desc=\'Open navigation drawer\']:";
+    public static final String HAMBURGER_MENU_DRAWER = "xpath://android.widget.ImageButton[@content-desc='Open navigation drawer']:";
     public static final String VIEW_NOT_DISPLAYED_MESSAGE = "Home page is not displayed.";
 
     public static final String SETTINGS_OPTION = "xpath:/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/androidx.drawerlayout.widget.DrawerLayout/android.widget.FrameLayout/androidx.recyclerview.widget.RecyclerView/androidx.appcompat.widget.LinearLayoutCompat[3]/android.widget.CheckedTextView";
